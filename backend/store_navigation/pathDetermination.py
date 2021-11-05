3350cfc73890adc08b577c4b5eca0d884cf1aee9
kitfagan <kit.fagan@publicissapient.com>
2021-11-05 11:41:54 +0000

    shortest path between nodes implemented but NOT TESTED

--- backend/store_navigation/pathDetermination.py
+++ backend/store_navigation/pathDetermination.py
@@ -9,3 +9,47 @@ def load_store():
 
     return (layout, metadata)
 
+def find_shortest_path(matrix, start_node, end_node):
+
+    distanceToNode = []
+    previousNode = []
+    for i in range(matrix.shape[0]):
+        distanceToNode.append(float('inf'))
+        previousNode.append(None)
+
+    distanceToNode[start_node] = 0
+    unopptimisedNodes = list(range(matrix.shape[0]))
+
+    while end_node in unopptimisedNodes:
+        nodeWithMinDistance = None
+        currentMinDistance = float('inf')
+        for node in unopptimisedNodes:
+            if distanceToNode[node] < currentMinDistance:
+                currentMinDistance = distanceToNode[node]
+                nodeWithMinDistance = node
+
+        unopptimisedNodes.remove(nodeWithMinDistance)
+        if nodeWithMinDistance == end_node:
+            break
+
+        headersToFilter = [str(i) for i in unopptimisedNodes]
+        connectionsToNeighbours = matrix.loc[nodeWithMinDistance, headersToFilter]
+        connectionsToNeighbours.dropna(inplace=True)
+        connectionsToNeighbours = pd.to_numeric(connectionsToNeighbours)
+        neighbours = list(connectionsToNeighbours.index.values)
+
+        for i in range(neighbours.length):
+            newDistance = currentMinDistance + connectionsToNeighbours[i]
+            if newDistance < distanceToNode[int(neighbours[i])]:
+                distanceToNode[int(neighbours[i])] = newDistance
+                previousNode[int(neighbours[i])] = nodeWithMinDistance
+
+    lastNodeInPath = previousNode[end_node]
+    pathToEnd = []
+    while lastNodeInPath != start_node:
+        pathToEnd.preppend(lastNodeInPath)
+        lastNodeInPath = previousNode[lastNodeInPath]
+
+    pathToEnd.preppend(start_node)
+
+    return (pathToEnd, distanceToNode[end_node])
