e23135e3342be9cc4825c4269d8c9a4b37d52fe0
jamallin <james.allington@publicissapient.com>
2021-11-09 11:13:28 +0000

    Jiggered the code around so now the state lies  in TabTwo rather than shoppingList index allowing us to pass props to the search bar

--- app/my-app/components/ListItem.tsx
+++ app/my-app/components/ListItem.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { View, Text, StyleSheet, TouchableHighlight } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
+import ShoppingList from '../shoppinglist';
 
 interface FuncProps{
     name:string;
